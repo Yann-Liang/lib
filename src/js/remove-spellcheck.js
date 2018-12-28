a01bea29169d9c328e0e56aba351c5e0dd8a97d8
liangyanxiang <liangyanxiang@juzix.io>
2018-12-28 16:37:47 +0800

    移除元素的拼写检查 入参不能为空

--- src/js/remove-spellcheck.js
+++ src/js/remove-spellcheck.js
@@ -1,8 +1,10 @@
 /**
  * 移除元素的拼写检查
+ * eleName string 标签元素
  */
 
 export default (eleName) => {
+    if (!eleName) throw Error('eleName 不能为空')
     const doms = document.getElementsByTagName(eleName);
     for (const item of doms) {
         item.setAttribute('spellcheck', false);
